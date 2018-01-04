fc2bd753397fdfd2e57bfe96ef34fac87ca270f0
txw <txwgoogol@gmail.com>
2018-01-04 20:31:43 +0800

    增加原型模式

---
+++ app/src/test/java/com/txw/designpattern/prototype/Client.java
@@ -0,0 +1,59 @@
+package com.txw.designpattern.prototype;
+
+import org.junit.Test;
+
+/**
+ * 简单的原型模式
+ * Created by txw on 2018/1/4.
+ */
+public class Client {
+
+    /**
+     * 简单的原型模式
+     */
+    @Test
+    public void main() {
+        //构建文档对象
+        WordDocument originDoc = new WordDocument();
+        //编辑文档，添加图片
+        originDoc.setText("这是一篇文档");
+        originDoc.addImages("图片1");
+        originDoc.addImages("图片2");
+        originDoc.addImages("图片3");
+        originDoc.showDocument();
+
+        //以原始文档为原型，拷贝一份副本
+        WordDocument doc2 = originDoc.clone();
+        doc2.showDocument();
+        //修改文档副本，不会影响原始文档
+        doc2.setText("这是修改过的Doc2文本");
+        doc2.showDocument();
+
+        originDoc.showDocument();
+    }
+
+    /**
+     * 浅拷贝
+     */
+    @Test
+    public void main1() {
+        //构建文档对象
+        WordDocument originDoc = new WordDocument();
+        //编辑文档，添加图片
+        originDoc.setText("这是一篇文档");
+        originDoc.addImages("图片1");
+        originDoc.addImages("图片2");
+        originDoc.addImages("图片3");
+        originDoc.showDocument();
+
+        //以原始文档为原型，拷贝一份副本
+        WordDocument doc2 = originDoc.clone();
+        //修改文档副本，不会影响原始文档
+        doc2.setText("这是修改过的Doc2文本");
+        doc2.addImages("呵呵.jpg");
+        doc2.showDocument();
+
+        originDoc.showDocument();
+    }
+
+}
