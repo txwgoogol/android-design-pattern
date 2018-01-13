a245354f393c38894a185c911bea232537df89e3
txw <txwgoogol@gmail.com>
2018-01-13 18:50:54 +0800

    增加观察者模式

---
+++ app/src/main/java/com/txw/designpattern/observer/model/DevTechFrontier.java
@@ -0,0 +1,18 @@
+package com.txw.designpattern.observer.model;
+
+import java.util.Observable;
+
+/**
+ * 开发技术前线，这个网站是被观察者角色，当它有更新时所有的观察者（这里是程序员）都会收到通知。
+ * Created by txw on 2018/1/13.
+ */
+public class DevTechFrontier extends Observable {
+
+    public void postNewPublication(String content) {
+        //标识状态或内容发生改变
+        setChanged();
+        //通知所有观察者
+        notifyObservers(content);
+    }
+
+}
---
+++ app/src/test/java/com/txw/designpattern/observer/Client.java
@@ -0,0 +1,32 @@
+package com.txw.designpattern.observer;
+
+import com.txw.designpattern.observer.model.Coder;
+import com.txw.designpattern.observer.model.DevTechFrontier;
+
+import org.junit.Test;
+
+/**
+ * 观察者模式测试类
+ * Created by txw on 2018/1/12.
+ */
+public class Client {
+
+    @Test
+    public void main() {
+        //被观察的角色
+        DevTechFrontier devTechFrontier = new DevTechFrontier();
+        Coder coder = new Coder("mr.sample");
+        //观察者
+        Coder coder1 = new Coder("coder-1");
+        Coder coder2 = new Coder("coder-2");
+        Coder coder3 = new Coder("coder-3");
+        //将观察者注册到可观察对象的观察列表中
+        devTechFrontier.addObserver(coder);
+        devTechFrontier.addObserver(coder1);
+        devTechFrontier.addObserver(coder2);
+        devTechFrontier.addObserver(coder3);
+        //发布消息
+        devTechFrontier.postNewPublication("新的一起开发技术前线周报发布啦！");
+    }
+
+}
