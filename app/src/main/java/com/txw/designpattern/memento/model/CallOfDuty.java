80b369740645102326a60c46484801fef1e3fd4d
txw <txwgoogol@gmail.com>
2018-01-14 19:07:12 +0800

    增加备忘录模式

---
+++ app/src/main/java/com/txw/designpattern/memento/model/CallOfDuty.java
@@ -0,0 +1,64 @@
+package com.txw.designpattern.memento.model;
+
+/**
+ * 使命召唤游戏（数据模型可能不太合理，这里只做简单的演示）
+ * Created by txw on 2018/1/14.
+ */
+public class CallOfDuty {
+
+    private int mCheckpoint = 1;
+    private int mLifeValue = 100;
+    private String mWeapon = "沙漠之鹰";
+
+    //玩游戏
+    public void play() {
+        System.out.println("玩游戏:" + String.format("第%d关", mCheckpoint) + "奋战杀敌中");
+        mLifeValue = 10;
+        System.out.println("进度升级啦！");
+        mCheckpoint++;
+        System.out.println("达到:" + String.format("第%d关", mCheckpoint));
+    }
+
+    //退出游戏
+    public void quit() {
+        System.out.println("----------------");
+        System.out.println("退出前的游戏属性:" + this.toString());
+        System.out.println("退出游戏");
+        System.out.println("----------------");
+    }
+
+    /**
+     * 创建备忘录
+     *
+     * @return
+     */
+    public Memento createMemento() {
+        Memento memento = new Memento();
+        memento.mCheckpoint = mCheckpoint;
+        memento.mLifeValue = mLifeValue;
+        memento.mWeapon = mWeapon;
+        return memento;
+    }
+
+    /**
+     * 恢复游戏
+     *
+     * @param memento
+     */
+    public void restore(Memento memento) {
+        this.mCheckpoint = memento.mCheckpoint;
+        this.mLifeValue = memento.mLifeValue;
+        this.mWeapon = memento.mWeapon;
+        System.out.println("恢复后的游戏属性:" + this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "CallOfDuty{" +
+                "mCheckpoint=" + mCheckpoint +
+                ", mLifeValue=" + mLifeValue +
+                ", mWeapon='" + mWeapon + '\'' +
+                '}';
+    }
+
+}
---
+++ app/src/test/java/com/txw/designpattern/memento/Client.java
@@ -0,0 +1,34 @@
+package com.txw.designpattern.memento;
+
+import com.txw.designpattern.memento.model.CallOfDuty;
+import com.txw.designpattern.memento.model.Creataker;
+
+import org.junit.Test;
+
+/**
+ * 测试类
+ * Created by txw on 2018/1/12.
+ */
+public class Client {
+
+    @Test
+    public void main() {
+        //构建游戏对象
+        CallOfDuty callOfDuty = new CallOfDuty();
+        //打游戏
+        callOfDuty.play();
+
+        Creataker creataker = new Creataker();
+        //游戏存档
+        creataker.archive(callOfDuty.createMemento());
+
+        //退出游戏
+        callOfDuty.quit();
+
+        CallOfDuty callOfDuty1 = new CallOfDuty();
+        //恢复游戏
+        callOfDuty1.restore(creataker.getMemento());
+
+    }
+
+}
