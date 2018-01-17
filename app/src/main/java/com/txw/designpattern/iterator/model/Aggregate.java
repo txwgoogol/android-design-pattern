fce7e2517ed39b094fb37bdad3c000a0a5b9aea3
txw <txwgoogol@gmail.com>
2018-01-17 16:41:18 +0800

    增加迭代器模式

---
+++ app/src/main/java/com/txw/designpattern/iterator/model/Aggregate.java
@@ -0,0 +1,29 @@
+package com.txw.designpattern.iterator.model;
+
+/**
+ * 容器接口 提供创建具体迭代器角色接口
+ * Created by txw on 2018/1/17.
+ */
+public interface Aggregate<T> {
+
+    /**
+     * 添加一个元素
+     *
+     * @param obj 元素对象
+     */
+    void add(T obj);
+
+    /**
+     * 移除一个元素
+     *
+     * @param obj 元素对象
+     */
+    void remove(T obj);
+
+    /**
+     * 获取容器迭代器
+     *
+     * @return 迭代对象
+     */
+    Iterator<T> iterator();
+}
--- app/src/test/java/com/txw/designpattern/iterator/Client.java
+++ app/src/test/java/com/txw/designpattern/iterator/Client.java
@@ -1,5 +1,9 @@
 package com.txw.designpattern.iterator;
 
+import com.txw.designpattern.iterator.model.Aggregate;
+import com.txw.designpattern.iterator.model.ConcreteAggregate;
+import com.txw.designpattern.iterator.model.Iterator;
+
 import org.junit.Test;
 
 /**
@@ -10,7 +14,15 @@ public class Client {
 
     @Test
     public void main() {
-
+        Aggregate<String> a = new ConcreteAggregate<>();
+        a.add("Aige");
+        a.add("Studio \n");
+        a.add("SM");
+        a.add("Brother");
+        Iterator<String> i = a.iterator();
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
     }
 
 }
