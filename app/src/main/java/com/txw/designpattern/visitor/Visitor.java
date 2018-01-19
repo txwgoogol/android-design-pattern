442ed10733890d3da62e97a5c43f22c3d003a305
txw <txwgoogol@gmail.com>
2018-01-19 21:08:16 +0800

    增加访问者模式

--- app/src/main/java/com/txw/designpattern/visitor/Visitor.java
+++ app/src/main/java/com/txw/designpattern/visitor/Visitor.java
@@ -1,10 +1,11 @@
 package com.txw.designpattern.visitor;
 
 /**
- * 访问者模式
+ * 访问者模式 定义：封装一些作用于某种数据结构中的各种元素的操作，它可以不改变这个数据结构的前提下定义左右于这些元素的系的操作。
  *
  * @author txw
  * @date 2017/11/8.
  */
 public class Visitor {
+
 }
