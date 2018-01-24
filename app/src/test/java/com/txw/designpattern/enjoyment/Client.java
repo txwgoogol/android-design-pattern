502492680182f32825bd1bfe59a1447ef82f9a6c
txw <txwgoogol@gmail.com>
2018-01-24 21:54:08 +0800

    增加享原模式

---
+++ app/src/test/java/com/txw/designpattern/enjoyment/Client.java
@@ -0,0 +1,16 @@
+package com.txw.designpattern.enjoyment;
+
+import org.junit.Test;
+
+/**
+ * 享原模式测试类
+ * Created by txw on 2018/1/12.
+ */
+public class Client {
+
+    @Test
+    public void main() {
+
+    }
+
+}
