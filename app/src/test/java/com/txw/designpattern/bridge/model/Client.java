48141a35939884dfff58afa4c8308a28118711df
txw <txwgoogol@gmail.com>
2018-02-06 10:35:44 +0800

    增加乔接模式

---
+++ app/src/test/java/com/txw/designpattern/bridge/model/Client.java
@@ -0,0 +1,27 @@
+package com.txw.designpattern.bridge.model;
+
+import org.junit.Test;
+
+/**
+ * 测试类
+ * Created by txw on 2018/1/12.
+ */
+public class Client {
+
+    @Test
+    public void main() {
+
+        ConcreteImplementorA implementorA = new ConcreteImplementorA();
+        //implementorA.operationImpl();
+
+        ConcreteImplementorB implementorB = new ConcreteImplementorB();
+        //implementorB.operationImpl();
+
+        Abstraction abstractionA = new RefinedAbstraction(implementorA);
+        abstractionA.operation();
+
+        Abstraction abstractionB = new RefinedAbstraction(implementorB);
+        abstractionB.operation();
+    }
+
+}
